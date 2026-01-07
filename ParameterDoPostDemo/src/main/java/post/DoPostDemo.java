42de8d93013863459034753f96d5fea11c749f69
Dhiraj093328 <162727053+Dhiraj093328@users.noreply.github.com>
2026-01-07 12:04:09 +0530

    DOGetDOPost and Seesion Tracking

--- ParameterDoPostDemo/src/main/java/post/DoPostDemo.java
+++ ParameterDoPostDemo/src/main/java/post/DoPostDemo.java
@@ -1,4 +1,4 @@
-package com;
+package post;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -9,16 +9,16 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
- * Servlet implementation class ParameterServlet
+ * Servlet implementation class DoPostDemo
  */
-@WebServlet("/ParameterServlet")
-public class ParameterServlet extends HttpServlet {
+@WebServlet("/DoPostDemo")
+public class DoPostDemo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ParameterServlet() {
+    public DoPostDemo() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -32,10 +32,13 @@ public class ParameterServlet extends HttpServlet {
 		PrintWriter pw=response.getWriter();
 		
 		String name=request.getParameter("username");
+
 		
 		pw.print("<html><body>");
+		pw.print("<h1>Using doGet</h1>");
 		pw.print("<h2>Welcome "+name+"</h2>");
-		pw.print("</body></html>");
+		pw.print("</body></html>");	
+		
 	}
 
 	/**
@@ -43,7 +46,18 @@ public class ParameterServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		response.setContentType("text/html");
+		PrintWriter pw=response.getWriter();
+		
+		String name=request.getParameter("username");
+		String pass=request.getParameter("upass");
+
+		
+		pw.print("<html><body>");
+		pw.print("<h1>Using doPost</h1>");
+		pw.print("<h2>Welcome "+name+"</h2>");
+		pw.print("<h4>Your Password is "+pass+"</h4>");
+		pw.print("</body></html>");	}
+	
 
 }
--- SessionTrackingDemo/src/main/java/com/FirstServlet.java
+++ SessionTrackingDemo/src/main/java/com/FirstServlet.java
@@ -2,6 +2,7 @@ package com;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,41 +10,46 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
- * Servlet implementation class ParameterServlet
+ * Servlet implementation class FirstServlet
  */
-@WebServlet("/ParameterServlet")
-public class ParameterServlet extends HttpServlet {
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ParameterServlet() {
+    public FirstServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.setContentType("text/html");
-		PrintWriter pw=response.getWriter();
-		
-		String name=request.getParameter("username");
-		
-		pw.print("<html><body>");
-		pw.print("<h2>Welcome "+name+"</h2>");
-		pw.print("</body></html>");
-	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		try {
+			response.setContentType("text/html");
+			PrintWriter pw=response.getWriter();
+			
+			String n=request.getParameter("username");
+			pw.print("Welcome "+n);
+			
+			Cookie ck=new Cookie("uname",n);
+			response.addCookie(ck);
+			
+			pw.print("<form action='SecondServlet' method='post' >");
+			pw.print("<input type='Submit' value='visit'>");
+			pw.print("</form>");
+			pw.close();
+			
+		} catch (Exception e)
+		{
+			System.out.println(e);
+		}
+		
 	}
 
 }
