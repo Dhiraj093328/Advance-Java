0bb1b26311c94730cdaf2e1d043462567770c385
Dhiraj093328 <162727053+Dhiraj093328@users.noreply.github.com>
2026-01-09 12:10:35 +0530

    RequestDispatcherPart-2,ServletConfig

---
+++ ServletConfigTest/src/main/java/com/ServletConfigClass.java
@@ -0,0 +1,43 @@
+package com;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class SevletConfigClass
+ */
+//@WebServlet("/SevletConfigClass")
+public class ServletConfigClass extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletConfigClass() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter pw=response.getWriter();
+		
+		ServletConfig Config=getServletConfig();
+		String n=Config.getInitParameter("name");
+		String s=Config.getInitParameter("surname");
+
+		pw.print("Your init parameter is: "+n+" "+s);
+		pw.close();
+	}
+
+}
---
+++ ServletConfigTest/src/main/java/com/ServletTwo.java
@@ -0,0 +1,42 @@
+package com;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class ServletTwo
+ */
+//@WebServlet("/ServletTwo")
+public class ServletTwo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletTwo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter pw=response.getWriter();
+		
+		ServletConfig Config=getServletConfig();
+		String n=Config.getInitParameter("driver");
+		String s=Config.getInitParameter("settings");
+
+		pw.print("Your init parameter is: "+n+" "+s);
+		pw.close();
+	}
+
+}
