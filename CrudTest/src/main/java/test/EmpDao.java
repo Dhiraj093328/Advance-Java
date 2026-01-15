efc4b74fdda6b2b8ce89380deff05494b61a0f73
Dhiraj093328 <162727053+Dhiraj093328@users.noreply.github.com>
2026-01-15 21:11:52 +0530

    advance jdbc

--- CrudTest/src/main/java/test/EmpDao.java
+++ CrudTest/src/main/java/test/EmpDao.java
@@ -1,43 +1,48 @@
 package test;
-import java.util.*;
+
 import java.sql.*;
+import java.util.*;
 
 public class EmpDao {
-	
-	public static Connection getConnection() {
-		Connection con=null;
-		try {
-			
-			Class.forName("com.mysql.cj.jdbc.driver");
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/crudtestdb","root","Dhiraj@0933");
-		}catch(Exception e)
-		{
-			System.out.println(e);
-		}
-		return con;
-	}
-	
-	public static int save(Emp e)
-	{
-		int status=0;
-		try {
-			
-				Connection con=EmpDao.getConnection();
-				PreparedStatement ps=con.prepareStatement("insert into usertable(id,name,password,email,country)values (?,?,?,?,?)");
-				ps.setInt(1, e.getId());
-				ps.setString(1, e.getName());
-				ps.setString(2, e.getPassword());
-				ps.setString(1, e.getEmail());
-				ps.setString(1, e.getCountry());
-				
-				status=ps.executeUpdate();
-						con.close();
 
-		} catch(Exception e1)
-		{
-			System.out.println(e1);
-		}
-		return status;
-	}
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/crudtestdb",
+                "root",
+                "Dhiraj@0933"
+            );
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return con;
+    }
 
+    public static int save(Emp e) {
+        int status = 0;
+        try {
+            Connection con = EmpDao.getConnection();
+
+            PreparedStatement ps = con.prepareStatement(
+                "INSERT INTO usertable (id, name, password, email, country) VALUES (?, ?, ?, ?, ?)"
+            );
+
+            ps.setInt(1, e.getId());
+            ps.setString(2, e.getName());
+            ps.setString(3, e.getPassword());
+            ps.setString(4, e.getEmail());
+            ps.setString(5, e.getCountry());
+
+            status = ps.executeUpdate();
+
+            ps.close();
+            con.close();
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return status;
+    }
 }
