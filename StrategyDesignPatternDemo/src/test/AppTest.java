76ed0f119e8532ada064fdfc403ca06ec4f17ffd
Dhiraj093328 <162727053+Dhiraj093328@users.noreply.github.com>
2026-02-11 13:56:00 +0530

    Maven Project

--- StrategyDesignPatternDemo/src/test/AppTest.java
+++ StrategyDesignPatternDemo/src/test/AppTest.java
@@ -4,8 +4,11 @@ public class AppTest {
 
 	public static void main(String[] args) {
 		
+		//constructor Injection
 		ShoppingCart cart= new ShoppingCart();
 		
+		
+		//setter method Injection
 		cart.setPaymentStrategy(new CreditCardPayment());
 		cart.checkout(100);
 
--- StrategyDesignPatternDemo/src/test/ShoppingCart.java
+++ StrategyDesignPatternDemo/src/test/ShoppingCart.java
@@ -8,9 +8,9 @@ public class ShoppingCart {
 	public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
 		this.paymentStrategy = paymentStrategy;
 	}
-	public ShoppingCart(PaymentStrategy paymentStrategy) {
+	/*public ShoppingCart(PaymentStrategy paymentStrategy) {
 		this.paymentStrategy = paymentStrategy;
-	}
+	}*/
 	public void checkout(int amount) {
 		paymentStrategy.pay(amount);
 		
